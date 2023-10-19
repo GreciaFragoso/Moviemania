137f7ee8295436f9b4ece8f387dcba840c3f8713
Grecia Fragoso <naomistereo96@gmail.com>
2023-10-18 21:29:58 -0500

    header, footer, sidebar, cards container and buttons container components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,21 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieComponent } from './components/movie/movie.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { CardsContainerComponent } from './components/cards-container/cards-container.component';
+import { ButtonsContainerComponent } from './components/buttons-container/buttons-container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovieComponent
+    MovieComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    CardsContainerComponent,
+    ButtonsContainerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/buttons-container/buttons-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buttons-container',
+  templateUrl: './buttons-container.component.html',
+  styleUrls: ['./buttons-container.component.css']
+})
+export class ButtonsContainerComponent {
+
+}
